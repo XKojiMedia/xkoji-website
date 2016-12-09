daae143727db2676f892d91b3b27eaff184de5cb
Samuel Imolorhe <samuelimolo4real@gmail.com>
2016-12-09 19:43:36 +0100

    Refactored the splash page using blade templates.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -24,24 +24,6 @@
   <meta name="msapplication-TileColor" content="#ffffff">
   <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
   <meta name="theme-color" content="#ffffff">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body class="fixed-height">
-<div class="wrapper">
-  <div class="copyright-wrapper">
-    XKojiMedia &copy; 2017
-  </div>
-</div>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-41432833-3', 'auto');
-  ga('send', 'pageview');
-
-</script>
-</body>
-</html>
+<body>
---
+++ resources/views/site/splash.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="splash-wrapper">
+      <div class="inner-wrapper">
+        <div class="logo-wrapper">
+          <img src="img/logo.png" alt="">
+        </div>
+        <div class="slogan-wrapper">
+          Creativity. Innovation. Design.
+        </div>
+      </div>
+      <div class="contact-link-wrapper">
+        <a href="mailto:admin@xkoji.com.ng">Wanna say hi?</a>
+      </div>
+    </div>
+@endsection
