8b4725a7cb66fb6465f86df5aeeced55165b0fdb
Samuel Imolorhe <samuelimolo4real@gmail.com>
2016-12-10 22:17:45 +0100

    Added social media links section to homepage.

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -17,7 +17,7 @@
           <div class="jumbo-box box-bottom"></div>
         </div>
         <div class="jumbo-vp">
-          <h1>
+          <h1 class="jumbo-vp-text">
             <span class="vp-creativity">Creativity.</span>
             <span class="vp-design">Design.</span>
             <span class="vp-innovation">Innovation.</span>
@@ -25,45 +25,83 @@
         </div>
       </div>
 
-      <div class="services-section">
-        <div class="services-bg"></div>
-        <div class="container">
-          <div class="service-item">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="service-image">
+      <div class="content-section">
+        <div class="content-bg"></div>
+        <div class="services-section">
+          <div class="container">
+            <div class="service-item">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="service-image">
 
+                  </div>
                 </div>
-              </div>
-              <div class="col-md-6">
-                <h2>Consult</h2>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aut, blanditiis corporis doloribus ipsum molestias, officia, officiis provident sit temporibus ullam voluptatibus. Blanditiis doloribus enim laborum nihil provident sapiente ullam.
-              </div>
-            </div>
-          </div>
-          <div class="service-item">
-            <div class="row">
-              <div class="col-md-6">
-                <h2>Consult</h2>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aut, blanditiis corporis doloribus ipsum molestias, officia, officiis provident sit temporibus ullam voluptatibus. Blanditiis doloribus enim laborum nihil provident sapiente ullam.
-              </div>
-              <div class="col-md-6">
-                <div class="service-image">
+                <div class="col-md-6">
+                  <h2>Consult</h2>
+                  <p>
+                    We help you along the way in implementing the not so easy tasks required by your business. We give you all the tools and tricks you would need to make you successful in whatever you do.
 
+                    Website optimisation
+                    SEO audits
+                  </p>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="service-item">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="service-image">
+            <div class="service-item">
+              <div class="row">
+                <div class="col-md-6">
+                  <h2>Consult</h2>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aut, blanditiis corporis doloribus ipsum molestias, officia, officiis provident sit temporibus ullam voluptatibus. Blanditiis doloribus enim laborum nihil provident sapiente ullam.
+                </div>
+                <div class="col-md-6">
+                  <div class="service-image">
 
+                  </div>
                 </div>
               </div>
-              <div class="col-md-6">
-                <h2>Consult</h2>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aut, blanditiis corporis doloribus ipsum molestias, officia, officiis provident sit temporibus ullam voluptatibus. Blanditiis doloribus enim laborum nihil provident sapiente ullam.
+            </div>
+            <div class="service-item">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="service-image">
+
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <h2>Consult</h2>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aut, blanditiis corporis doloribus ipsum molestias, officia, officiis provident sit temporibus ullam voluptatibus. Blanditiis doloribus enim laborum nihil provident sapiente ullam.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="social-media-section">
+          <h2 class="social-media-section-title">Let us know what you think</h2>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="social-media-links">
+                  <a href="#" class="media-item">
+                    <div class="media-item-content">
+                      Facebook
+                    </div>
+                  </a>
+                  <a href="#" class="media-item">
+                    <div class="media-item-content">
+                      Twitter
+                    </div>
+                  </a>
+                  <a href="#" class="media-item">
+                    <div class="media-item-content">
+                      LinkedIn
+                    </div>
+                  </a>
+                  <a href="#" class="media-item">
+                    <div class="media-item-content">
+                      About.me
+                    </div>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
